da271a8249544d7fd56102ac8a415629f9c8add4
Joj <jannahashraf10@gmail.com>
2025-05-02 21:10:49 +0300

    Update Student.java

--- src/main/java/edu/nu/sgm/models/Student.java
+++ src/main/java/edu/nu/sgm/models/Student.java
@@ -1,1 +1,32 @@
-class Student {{}
+public class Student {
+    private String name;
+    private double id;
+    private String enrolled_courses;
+    
+    Student(String name, double id, String enrolled_courses){
+        this.name=name;
+        this.id=id;
+        this.enrolled_courses=enrolled_courses;
+    }
+
+    public void setname(String name) {
+        this.name=name;
+     }
+     public void setid(double id) {
+        this.id=id;
+     }
+     public void setenrolled_courses(String enrolled_courses) {
+        this.enrolled_courses=enrolled_courses;
+     }
+     
+     public String getname() {
+        return name;
+     }
+     public double getid() {
+        return id;
+     }
+     public String getenrolled_courses() {
+        return enrolled_courses;
+     }
+
+}
