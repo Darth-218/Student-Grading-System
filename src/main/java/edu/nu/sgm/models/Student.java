4e41156845426464958e6b0b4bf4409b5601bd6f
darth-218 <yahia.hany.gaber@proton.me>
2025-05-14 22:53:13 +0300

    Updated student constructor

--- src/main/java/edu/nu/sgm/models/Student.java
+++ src/main/java/edu/nu/sgm/models/Student.java
@@ -10,7 +10,7 @@ public class Student {
     this.id = id;
     this.name = name;
     this.last_name = last_name;
-    this.email = generateStudentEmail();
+    this.email = email;
   }
 
   public int getId() { return id; }
