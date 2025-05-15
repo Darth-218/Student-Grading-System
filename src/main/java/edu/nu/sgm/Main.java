4e0d917dee866c3972259d3e2762983623c7bcc5
ahmedmaboud <ahmedmaboud2005@gmail.com>
2025-05-16 01:11:54 +0300

    Fixed path for main-view.fxml

--- src/main/java/edu/nu/sgm/Main.java
+++ src/main/java/edu/nu/sgm/Main.java
@@ -7,19 +7,18 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
-  public static void main(String[] args) { 
+  public static void main(String[] args) {
     launch(args);
   }
 
   @Override
   public void start(Stage stage) {
     try {
-      Parent root = FXMLLoader.load(getClass().getResource("main-view.fxml"));
+      Parent root = FXMLLoader.load(getClass().getResource("/edu/nu/sgm/views/main-view.fxml"));
       Scene scene = new Scene(root);
       stage.setScene(scene);
       stage.show();
-    }
-    catch(Exception e) {
+    } catch (Exception e) {
       e.printStackTrace();
     }
   }
